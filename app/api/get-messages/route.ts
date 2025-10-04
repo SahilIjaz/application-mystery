44de33e2c2882c179572a5559c1304ace9733653
sahil Ijaz <hssahil2913@gmail.com>
2025-10-05 00:24:34 +0500

    9

--- app/api/get-messages/route.ts
+++ app/api/get-messages/route.ts
@@ -5,7 +5,6 @@ import NextAuth from "next-auth";
 import { User } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/options";
 import { fail } from "assert";
-
 import { acceptingMessageSchema } from "@/schemas/acceptingMessageSchema";
 import mongoose from "mongoose";
 
