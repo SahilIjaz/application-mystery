9732da60bb7a376b4f83373ff0c89abee1aacf52
sahil Ijaz <hssahil2913@gmail.com>
2025-10-04 23:09:33 +0500

    designing doene

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -12,18 +12,20 @@ function dashboard() {
     const user: User = session?.user as User;
 
     return (
-        <nav>
-            <div>
-                <a href="#">mysteryMessage</a>
+        <nav className='p-5 bg-gray-200 flex justify-between items-center'>
+            <div className='container mx-auto flex justify-between items-center md:px-20'>
+                <a className='font-bold text-2xl mb-4 mb:mb-0'
+                    href="#">mysteryMessage</a>
 
                 {
                     session ? (
                         <>
-                            <span>Welcome, {user.userName || user.email}</span>
-                            <Button onClick={() => signOut}>LogOut</Button>
+                            <span className='mr-4'>Welcome, {user.userName || user.email}</span>
+                            <Button className='bg-red-500 hover:bg-red-600 text-white'
+                                onClick={() => signOut}>LogOut</Button>
                         </>
                     ) : (
-                        <Link href="/sign-in">SignIn</Link>
+                        <Link href="/sign-in"><Button className='bg-red-500 hover:bg-red-600 text-white'>LogIn</Button></Link>
                     )
                 }
             </div>
