b47366501e2d43473558bf7d7ab617948e201ec6
sahil Ijaz <hssahil2913@gmail.com>
2025-10-05 00:24:14 +0500

    7

--- app/api/delete-message/[messageId]/route.ts
+++ app/api/delete-message/[messageId]/route.ts
@@ -3,7 +3,6 @@ import { UserModel } from "@/models/User";
 
 import { getServerSession } from "next-auth";
 import NextAuth from "next-auth";
-
 import { User } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
 import { fail } from "assert";
