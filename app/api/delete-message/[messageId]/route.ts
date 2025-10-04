952f2061503993a0080bb86ca2e9dbb0d1842410
sahil Ijaz <hssahil2913@gmail.com>
2025-10-05 00:23:55 +0500

    prettier installed

--- app/api/delete-message/[messageId]/route.ts
+++ app/api/delete-message/[messageId]/route.ts
@@ -1,5 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
 import { UserModel } from "@/models/User";
+
 import { getServerSession } from "next-auth";
 import NextAuth from "next-auth";
 
@@ -9,56 +10,58 @@ import { fail } from "assert";
 import { acceptingMessageSchema } from "@/schemas/acceptingMessageSchema";
 import mongoose from "mongoose";
 
+export async function Delete(
+  request: Request,
+  { params }: { params: { messageId: string } }
+) {
+  const { messageId } = params;
+  await dbConnect();
 
-export async function Delete(request: Request, { params }: { params: { messageId: string } }) {
-    const { messageId } = params;
-    await dbConnect();
+  const session = await getServerSession(authOptions);
 
-    const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
 
-    const user: User = session?.user as User;
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        message: "User not authenticated.",
+        success: false,
+      },
+      { status: 401 }
+    );
+  }
 
-    if (!session || !session.user) {
-        return Response.json(
-            {
-                message: "User not authenticated.",
-                success: false,
-            },
-            { status: 401 }
-        );
+  const userId = new mongoose.Types.ObjectId(user._id);
+
+  try {
+    const updatedMessage = await UserModel.updateOne(
+      { _id: userId },
+      { $pull: { messages: { _id: new mongoose.Types.ObjectId(messageId) } } }
+    );
+    if (updatedMessage.modifiedCount === 0) {
+      return Response.json(
+        {
+          message: "Message not found.",
+          success: false,
+        },
+        { status: 404 }
+      );
     }
 
-    const userId = new mongoose.Types.ObjectId(user._id);
-
-    try {
-        const updatedMessage = await UserModel.updateOne(
-            { _id: userId },
-            { $pull: { messages: { _id: new mongoose.Types.ObjectId(messageId) } } }
-        );
-        if (updatedMessage.modifiedCount === 0) {
-            return Response.json(
-                {
-                    message: "Message not found.",
-                    success: false,
-                },
-                { status: 404 }
-            );
-        }
-
-        return Response.json(
-            {
-                message: "Message deleted successfully.",
-                success: true,
-            },
-            { status: 200 }
-        );
-    } catch (error) {
-        return Response.json(
-            {
-                message: "Internal server error.",
-                success: false,
-            },
-            { status: 500 }
-        );
-    }
+    return Response.json(
+      {
+        message: "Message deleted successfully.",
+        success: true,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return Response.json(
+      {
+        message: "Internal server error.",
+        success: false,
+      },
+      { status: 500 }
+    );
+  }
 }
