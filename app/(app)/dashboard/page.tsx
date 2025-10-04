8723fb1225d37a3c4b62e92e5d0d9fcec614e5c4
sahil Ijaz <hssahil2913@gmail.com>
2025-10-04 23:06:09 +0500

    dashboard

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,8 +1,34 @@
+'use client'
+import { signOut, useSession } from 'next-auth/react'
 import React from 'react'
+import { User } from 'next-auth'
+import { Button } from '@/components/ui/button';
+import { Link } from 'lucide-react';
 
 function dashboard() {
+
+    const { data: session } = useSession();
+
+    const user: User = session?.user as User;
+
     return (
-        <div>dashboard</div>
+        <nav>
+            <div>
+                <a href="#">mysteryMessage</a>
+
+                {
+                    session ? (
+                        <>
+                            <span>Welcome, {user.userName || user.email}</span>
+                            <Button onClick={() => signOut}>LogOut</Button>
+                        </>
+                    ) : (
+                        <Link href="/sign-in">SignIn</Link>
+                    )
+                }
+            </div>
+        </nav>
+
     )
 }
 
